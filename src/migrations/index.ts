cb6a15ded9962ec9f112e536c972d355e228686f
nafweb1 <webmaster.nafweb@gmail.com>
2025-03-28 21:52:39 +0100

    Changed from Payload to Svelte

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250316_072429 from './20250316_072429';
-
-export const migrations = [
-  {
-    up: migration_20250316_072429.up,
-    down: migration_20250316_072429.down,
-    name: '20250316_072429'
-  },
-];
